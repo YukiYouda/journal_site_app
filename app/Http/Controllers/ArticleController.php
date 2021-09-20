1f2613eb64f9f419c8de90d9ca28b6ad23fae7f7
YukiYouda <youda0518@yahoo.co.jp>
2021-09-20 22:21:05 +0900

    新規登録機能の実装

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -13,6 +13,23 @@ class ArticleController extends Controller
         return view('articles.index', compact('articles'));
     }
 
+    public function create()
+    {
+        return view('articles.create');
+    }
+
+    public function store(Request $request)
+    {
+        $article = new Article;
+
+        $article->title = $request->title;
+        $article->body = $request->body;
+
+        $article->save();
+
+        return redirect('/articles');
+    }
+
     public function show($id)
     {
         $article = Article::find($id);
